4d43c0d21935a62aabbdc42aafdc6a631158492f
Nathan Frad Serli <nathanfrad86@gmail.com>
2020-09-30 18:10:59 +0200

    zone de recherche sur les pages trajets, historiques et prêts

--- src/app/protected/user/manage-ride/list-ride/list-ride.component.ts
+++ src/app/protected/user/manage-ride/list-ride/list-ride.component.ts
@@ -5,6 +5,8 @@ import {RestRideService} from "../../../../core/services/rest/rest-ride.service"
 import {ManagerRideService} from "../services/manager-ride.service";
 import {AuthenticationService} from '../../../../core/services/authentication/authentication.service';
 import * as moment from 'moment';
+import {isString} from "util";
+import {Car} from "../../../../shared/models/entities/car";
 
 @Component({
   selector: 'app-list-ride',
@@ -19,6 +21,8 @@ export class ListRideComponent implements OnInit {
    * @memberof ListRideComponent
    */
   rides: Ride[] = [];
+
+  ridessFiltered: Ride[] = [];
 
   /**
    * Creates an instance of ListRideComponent.
@@ -42,9 +46,9 @@ export class ListRideComponent implements OnInit {
    * @memberof ListRideComponent
    */
   ngOnInit() {
-    this.route.params.subscribe( params => {
-      if ( !params['rideType'] ) this.router.navigate(['/error']);
-      if ( params['rideType'] != 'current' && params['rideType'] != 'history' ) this.router.navigate(['/error']);
+    this.route.params.subscribe(params => {
+      if (!params['rideType']) this.router.navigate(['/error']);
+      if (params['rideType'] != 'current' && params['rideType'] != 'history') this.router.navigate(['/error']);
       const type: 'current' | 'history' = params['rideType'];
 
       this.getRides(type);
@@ -54,13 +58,13 @@ export class ListRideComponent implements OnInit {
 
   getRides(routeType: 'current' | 'history') {
     this.restRide.getRidesByLogin(this.userService.currentUserValue.id)
-      .subscribe( (rides: Ride[]) => {
+      .subscribe((rides: Ride[]) => {
         //this.rides = rides;
         if (rides) this.populateRides(rides, routeType);
         if (this.rides) this.rideService.changeRides(this.rides);
       });
     this.rideService.$rides
-      .subscribe( (rides: Ride[]) => {
+      .subscribe((rides: Ride[]) => {
         //this.rides = rides
         if (rides) this.populateRides(rides, routeType)
       });
@@ -69,21 +73,27 @@ export class ListRideComponent implements OnInit {
   populateRides(rides: Ride[], routeType: 'current' | 'history') {
     //if (rides) console.log('DATE RIDE: '+rides[0].arrivalDate);
     //if (rides) console.log('DATE RIDE MOMENT: '+moment(rides[0].arrivalDate).format('DD MM YYYY'));
-    if ( routeType == 'current' ) {
-      this.rides = rides.filter( r => r.status != 'REJECTED' ) .filter( r => moment(r.arrivalDate, 'YYYY-MM-DD').isSameOrAfter(moment(), 'day') );
+    if (routeType == 'current') {
+      this.rides = rides.filter(r => r.status != 'REJECTED').filter(r => moment(r.arrivalDate, 'YYYY-MM-DD').isSameOrAfter(moment(), 'day'));
+      this.ridessFiltered = rides.filter(r => r.status != 'REJECTED').filter(r => moment(r.arrivalDate, 'YYYY-MM-DD').isSameOrAfter(moment(), 'day'));
       this.rideSort(true);
-    } else
-    if ( routeType == 'history' ) {
-      this.rides = rides.filter( r => r.status == 'REJECTED' || (r.status != 'REJECTED' && moment(r.arrivalDate, 'YYYY-MM-DD').isBefore(moment(), 'day')) );
+    } else if (routeType == 'history') {
+      this.rides = rides.filter(r => r.status == 'REJECTED' || (r.status != 'REJECTED' && moment(r.arrivalDate, 'YYYY-MM-DD').isBefore(moment(), 'day')));
+      this.ridessFiltered = rides.filter(r => r.status == 'REJECTED' || (r.status != 'REJECTED' && moment(r.arrivalDate, 'YYYY-MM-DD').isBefore(moment(), 'day')));
+
       this.rideSort(false);
-    } else this.rides = [];
+    } else {
+      this.rides = [];
+      this.ridessFiltered = [];
+    }
+
   }
 
-  rideSort(chronoSort: boolean){
-    this.rides = this.rides.sort( (r1, r2) => 
-        moment(r1.departureDate, 'YYYY-MM-DD') > moment(r2.departureDate, 'YYYY-MM-DD') ? (chronoSort ? 1 : -1) :
+  rideSort(chronoSort: boolean) {
+    this.ridessFiltered = this.rides.sort((r1, r2) =>
+      moment(r1.departureDate, 'YYYY-MM-DD') > moment(r2.departureDate, 'YYYY-MM-DD') ? (chronoSort ? 1 : -1) :
         moment(r1.departureDate, 'YYYY-MM-DD') < moment(r2.departureDate, 'YYYY-MM-DD') ? (chronoSort ? -1 : 1) : 0
-      );
+    );
   }
 
   /**
@@ -112,6 +122,33 @@ export class ListRideComponent implements OnInit {
    */
   onClickClose() {
     this.router.navigate(['/public']); //TODO navigate to home
+  }
+
+  /**
+   * Gestion de l'évenement ajout d'une entrée dans la zone de recherche
+   * Filtre la liste des sites (minium 3 caractères à saisir dans le champ)
+   * @param {Place} place Un lieu
+   * @memberof ListPlaceComponent
+   */
+  onInputSearch(event: any) {
+
+    try {
+
+      if (!isString(event.toString())) throw new Error();
+
+      const inputValue: string = event.trim().toLocaleLowerCase();
+
+      if (inputValue.length >= 3) {
+        this.ridessFiltered = this.rides.filter(
+          ride => (ride.departureSite.siteName.toLocaleLowerCase().search(inputValue) > -1 || ride.arrivalSite.siteName.toLocaleLowerCase().search(inputValue) > -1 || ride.arrivalDate.toLocaleLowerCase().search(inputValue) > -1 || ride.departureDate.toLocaleLowerCase().search(inputValue) > -1 )
+        );
+      } else {
+        throw new Error();
+      }
+
+    } catch {
+    }
+
   }
 
 }
